34b99f5a9383cdb375fe880be4b6f93ca8738c7c
addpadding <tiktokaddison20@gmail.com>
2023-03-24 03:59:14 +0300

    part 4 = End

--- app.js
+++ app.js
@@ -19,29 +19,46 @@ var paginationElement = document.createElement("ul")
 //set ID Created Ul Element
 paginationElement.setAttribute("id", "pagination-ul")
 
-// Create List Items Based On Slide Count
+// Create List Items Based On Slide Count {اضافة الازرار}
 for (var i = 1; i <= slidesCount; i++) {
-    // Create The Li
-    var paginationItem = document.createElement("li")
-    //set Custom Attribute
-    paginationItem.setAttribute("data-index", i)
-    // Set item content
-    paginationItem.appendChild(document.createTextNode(i))
-    // Append item to the main ul list
-    paginationElement.appendChild(paginationItem)
+	// Create The Li
+	var paginationItem = document.createElement("li")
+	//set Custom Attribute
+	paginationItem.setAttribute("data-index", i)
+	// Set item content
+	paginationItem.appendChild(document.createTextNode(i))
+	// Append item to the main ul list
+	paginationElement.appendChild(paginationItem)
 }
 
 // Add the created ul element to the page
 document.getElementById("indicators").appendChild(paginationElement)
 
+// get the new created ul 
+var paginationCreatedUl = document.getElementById("pagination-ul")
+
+// trigger the Checker function
+theChecker()
+
 // Next Slide Function
-function nextSlide() {
-    console.log("next");
+function nextSlide(item) {
+	console.log("next");
 }
 
 // previous Slide Function
 function prevSlide() {
-    console.log("previous");
-    // ()
+	console.log("previous");
+}
 
+
+// creaat the checker function {اضافة لون على الازرار}
+function theChecker() {
+	// set the slide number
+	slideNumberElement.textContent = "slide #" + (currentSlide) + " of " + (slidesCount);
+
+	// set active class on current slide
+	sliderImages[currentSlide - 1].classList.add("active")
+
+	// set active class on current Pagination item
+	paginationCreatedUl.children[currentSlide - 1].classList.add("active")
 }
